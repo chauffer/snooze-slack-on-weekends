3bad38f424a7035fec3f8563cc4c128eec47aab0
Simone Esposito <chaufnet@gmail.com>
2018-03-10 16:56:55 +0100

    Use Docker instead of Lambda

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # snooze-slack-on-weekends
 
 Stop your team from bugging you when it's the freakin' weekend (or a holiday)!
+
+## Configuration
+The only supported way to run this is through Docker, it'll work otherwise, but you are on your own as to how to do this.
+
+### Environment variables
+- `SLACK_API_TOKEN` - Legacy slack token, starting with `xoxp-`
--- index.js
+++ index.js
@@ -5,11 +5,9 @@ const { isHoliday } = require('wgl-holidays');
 const isWeekday = new Date().getDay() !== 0 && new Date().getDay() !== 6;
 const isBusinessDay = isWeekday && !isHoliday(moment());
 
-module.exports.handler = (event, context, callback) => {
-  const client = new WebClient(process.env.SLACK_API_TOKEN);
-  if (!isBusinessDay) {
-    client.dnd.setSnooze(1440, (err, res) => {
-      console.log(!err && res.ok ? 'Snooze set for 24 hours' : err);
-    });
-  }
-};
+const client = new WebClient(process.env.SLACK_API_TOKEN);
+if (!isBusinessDay) {
+  client.dnd.setSnooze(1440, (err, res) => {
+    console.log(!err && res.ok ? 'Snooze set for 24 hours' : err);
+  });
+}
